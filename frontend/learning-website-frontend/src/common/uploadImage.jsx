e083f1e0dfd58b81ffa1e6eb3d0a47142f694e54
Ashfak Azad Nafi <ashfakazad@iut-dhaka.edu>
2025-10-30 22:09:37 +0600

    image upload issue solved

--- frontend/learning-website-frontend/src/common/uploadImage.jsx
+++ frontend/learning-website-frontend/src/common/uploadImage.jsx
@@ -1,27 +1,3 @@
- import React from "react";
-// import axios from "axios";
-
- const uploadImage = async (img) => {
-  let imgUrl = null;
-  await axios
-   .get(import.meta.env.VITE_SERVER_DOMAIN + "/get-upload-url")
-    .then(async ({ data: { uploadURL } }) => {
-       await axios({
-       method: "PUT",
-      url: uploadURL,
-       headers: {
-         "Content-Type": img.type || "application/octet-stream",
-       },
-       data: img,
-       timeout: 10000,
-     }).then(() => {
-       imgUrl = uploadURL.split("?")[0];
-     });
-   });
-  return imgUrl;
-};
-
-// export default uploadImage;
 
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { storage } from "./firebase"; // Make sure Firebase is properly configured
